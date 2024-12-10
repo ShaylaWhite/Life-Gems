1f44e8765c0f10b1e9b31acb196041cfecd885fd
Shayla White <shayladwhite1@gmail.com>
2024-12-10 15:56:28 -0800

    Initial commit

---
+++ gems-of-life/src/main/java/com/shaylawhite/gems_of_life/GemsOfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.shaylawhite.gems_of_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemsOfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemsOfLifeApplication.class, args);
+	}
+
+}
---
+++ gems-of-life/src/test/java/com/shaylawhite/gems_of_life/GemsOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaylawhite.gems_of_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemsOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
