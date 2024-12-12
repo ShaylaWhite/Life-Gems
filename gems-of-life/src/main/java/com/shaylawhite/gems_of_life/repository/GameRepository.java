0a628bcc4f26aae169d4c741756b108d534d6c11
Shayla White <shayladwhite1@gmail.com>
2024-12-12 15:48:40 -0800

    created the game repository interface

--- gems-of-life/src/main/java/com/shaylawhite/gems_of_life/repository/GameRepository.java
+++ gems-of-life/src/main/java/com/shaylawhite/gems_of_life/repository/GameRepository.java
@@ -1,4 +1,8 @@
 package com.shaylawhite.gems_of_life.repository;
 
-public class GameRepository {
+import com.shaylawhite.gems_of_life.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    // You can add custom queries here if needed, like finding a game by ID, etc.
 }
