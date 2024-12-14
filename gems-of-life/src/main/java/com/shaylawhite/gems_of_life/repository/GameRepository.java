999521ec8dfad8dda575c772f5ee4f9904ee8972
Shayla White <shayladwhite1@gmail.com>
2024-12-13 22:51:40 -0800

    updated the repository interface for the gaame entity

--- gems-of-life/src/main/java/com/shaylawhite/gems_of_life/repository/GameRepository.java
+++ gems-of-life/src/main/java/com/shaylawhite/gems_of_life/repository/GameRepository.java
@@ -2,6 +2,8 @@ package com.shaylawhite.gems_of_life.repository;
 
 import com.shaylawhite.gems_of_life.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 }
